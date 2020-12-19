7dd8c3a880945bdcd2dd7e2c4d034bc96e732ca4
kelsi-will <kelsi.n.williams@gmail.com>
2020-12-19 13:15:17 -0600

    Updated to correct icon URL

--- src/Weather.js
+++ src/Weather.js
@@ -12,7 +12,7 @@ function Weather({ defaultCity }) {
     city: response.data.name,
     date: new Date(response.data.dt * 1000),
     description: response.data.weather[0].description,
-    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+    iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     temperature: response.data.main.temp,
     humidity: response.data.main.humidity,
     wind: response.data.wind.speed,
